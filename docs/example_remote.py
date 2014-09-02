1a4e8dda0b1aab8cde973aecc7c77a40f1ac2655
astaric <anze.staric@gmail.com>
2014-09-02 13:48:47 +0200

    New syntax for remote imports.

--- docs/example_remote.py
+++ docs/example_remote.py
@@ -1,5 +1,7 @@
-import orangecontrib.remote as Orange
-from orangecontrib.remote.classification.logistic_regression import LogisticRegressionLearner
+from orangecontrib import remote
+with remote.server('127.0.0.1:9465'):
+    import Orange
+    from Orange.classification.logistic_regression import LogisticRegressionLearner
 
 iris = Orange.data.Table('iris')
 print(iris)
--- orangecontrib/remote/__init__.py
+++ orangecontrib/remote/__init__.py
@@ -1,22 +1,24 @@
+from contextlib import contextmanager
 import inspect
 import pkgutil
 import importlib
-import warnings
 import sys
 import imp
 
 import Orange
 from orangecontrib.remote.proxy import Proxy, get_server_address, wrapped_function, wrapped_member, create_proxy
 
-
 proxies = imp.new_module('proxies')
 sys.modules['proxies'] = proxies
 
 old_to_new = {}
+remote_orange_modules = {}
 
 
-def replace_orange_with_proxies():
+def create_remote_orange_module():
     excluded_modules = ["Orange.test", "Orange.canvas", "Orange.widgets"]
+
+    remote_orange = create_submodule(None, 'Orange')
     for importer, modname, ispkg in pkgutil.walk_packages(path=Orange.__path__, prefix="Orange.",
                                                           onerror=lambda x: None):
         if any(modname.startswith(excluded_module) for excluded_module in excluded_modules):
@@ -24,19 +26,11 @@ def replace_orange_with_proxies():
         try:
             module = importlib.import_module(modname)
 
-            new_module = globals()
-            module_name = __name__
-            for part in modname.lstrip("Orange.").split("."):
-                if new_module is globals():
-                    if part not in new_module:
-                        new_module[part] = imp.new_module(part)
-                    new_module = new_module[part]
-                else:
-                    if not hasattr(new_module, part):
-                        setattr(new_module, part, imp.new_module(part))
-                    new_module = getattr(new_module, part)
-                module_name = '.'.join((module_name, part))
-                sys.modules[module_name] = new_module
+            current_module = remote_orange
+            for submodule in modname.lstrip("Orange.").split("."):
+                if not hasattr(current_module, submodule):
+                    create_submodule(current_module, submodule)
+                current_module = getattr(current_module, submodule)
 
             for name, class_ in inspect.getmembers(module, inspect.isclass):
                 if not class_.__module__.startswith("Orange"):
@@ -49,17 +43,32 @@ def replace_orange_with_proxies():
                     old_to_new[class_] = new_class
                     setattr(proxies, new_name, new_class)
 
-                if new_module is globals():
-                    new_module[name] = new_class
-                else:
-                    setattr(new_module, name, new_class)
+                setattr(current_module, name, new_class)
 
         except ImportError as err:
             sys.stderr.write("Failed to load module %s: %s\n" % (modname, err))
+    return remote_orange
 
 
-replace_orange_with_proxies()
+def create_submodule(parent, name):
+    module = imp.new_module(name)
+    if parent is not None:
+        setattr(parent, name, module)
+        name = '.'.join((parent.__name__, name))
+    module.__name__ = name
+    remote_orange_modules[name] = module
+    return module
 
-del Orange
+@contextmanager
+def server(address):
+    if address is None:
+        yield
+    else:
+        old_modules = sys.modules.copy()
+        sys.modules.update(remote_orange_modules)
+        yield
+        sys.modules = old_modules
+
+create_remote_orange_module()
 
 print("Using Orange Server %s:%s" % get_server_address())
