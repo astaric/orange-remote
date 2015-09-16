505994d3a768536bc71219d5e168d05fc1cd6b4d
astaric <anze.staric@gmail.com>
2015-09-16 16:36:30 +0200

    celery: Use results backend instead of storing them manually.

--- orangecontrib/remote/executor/celery.py
+++ orangecontrib/remote/executor/celery.py
@@ -1,15 +1,14 @@
 import os
-import pickle
+import threading
 
 from celery import Celery
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, LoggingEventHandler
+from celery.exceptions import TimeoutError
 
 from orangecontrib.remote.commands import ExecutionFailed
 from orangecontrib.remote.executor import Executor
 from orangecontrib.remote.results_manager import ResultsManager
 
-app = Celery()
+app = Celery(backend='amqp://', broker='amqp://')
 
 tmp_path = os.path.join(os.path.dirname(__file__), 'tmp')
 results_path = os.path.join(os.path.dirname(__file__), 'results')
@@ -19,35 +18,13 @@ class CeleryExecutor(Executor):
     def __init__(self):
         super().__init__()
         self.id_mapping = {}
-        self.observer = None
-
-    def start(self):
-        super.start()
-
-        class ResultsHandler(FileSystemEventHandler):
-            def on_created(this, event):
-                _, result_id = os.path.split(event.src_path)
-                self.logger.info("Received result " + result_id)
-
-                with open(event.src_path, 'rb') as f:
-                    result = pickle.load(f)
-
-                ResultsManager.set_result(result_id, result)
-
-        handler = ResultsHandler()
-
-        self.observer = Observer()
-        self.observer.schedule(handler, results_path)
-        self.observer.schedule(LoggingEventHandler(), results_path)
-        self.observer.start()
-
-    def stop(self):
-        super().stop()
-        self.observer.stop()
+        self.waiters = []
 
     def join(self):
         super().join()
-        self.observer.join()
+        for t in self.waiters:
+            self.logger.info("Joining %s" % t)
+            t.join()
 
     def _abort_command(self, command):
         celery_id = self.id_mapping.get(command.id, None)
@@ -56,21 +33,33 @@ class CeleryExecutor(Executor):
             result.abort()
 
     def _execute_command(self, command, result_id):
-        execute_command.delay(command, result_id)
+        promise = execute_command.delay(command)
+        self._await_result(result_id, promise)
 
+    def _await_result(self, result_id, promise):
+        t = threading.Thread(
+            name='Result getter',
+            target=self._wait_on_promise,
+            kwargs=dict(result_id=result_id, promise=promise)
+        )
+        t.start()
+        self.waiters.append(t)
+
+    def _wait_on_promise(self, result_id, promise):
+        while self._is_running:
+            try:
+                result = promise.get(timeout=1)
+                ResultsManager.set_result(result_id, result)
+                return
+            except TimeoutError:
+                pass
 
 @app.task
-def execute_command(command, result_id):
+def execute_command(command):
     try:
-        result = command.execute()
+        return command.execute()
     except Exception as e:
-        result = ExecutionFailed(command, e)
-    with open(os.path.join(tmp_path, result_id), 'wb') as f:
-        pickle.dump(result, f, pickle.HIGHEST_PROTOCOL)
-    os.rename(os.path.join(tmp_path, result_id),
-              os.path.join(results_path, result_id))
-    print("Stored result " + result_id)
-
+        return ExecutionFailed(command, e)
 
 if __name__ == '__main__':
     app.worker_main()
